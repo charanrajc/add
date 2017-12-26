e33d438aaabf25ac363280d50f316f3de53fd246
Charan Raj <Charan Raj@192.168.100.5>
2017-12-26 20:26:58 +0530

    intitaly checkin

---
+++ add/src/add/Demo.java
@@ -0,0 +1,9 @@
+package add;
+
+public class Demo 
+{
+ public double add(int a, int b)
+ {
+	 return a+b;
+ }
+}
---
+++ add/src/add/DemoClient.java
@@ -0,0 +1,10 @@
+package add;
+
+public class DemoClient 
+{
+	public static void main(String[] args)
+	{
+		Demo demo = new Demo();
+		System.out.println(demo.add(4, 6));
+	}
+}
